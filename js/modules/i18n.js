98b7a9d7d2b436195e5984b22ed82414b6ad0eb7
MarcioRosendoF <145620413+MarcioRosendoF@users.noreply.github.com>
2026-08-22 15:01:27 -0300

    chore(release): bump cache busting version to 2.8

--- js/modules/i18n.js
+++ js/modules/i18n.js
@@ -60,7 +60,7 @@ let translationTimeline = null;
 
 export async function loadTranslations(lang, isInitialLoad = false) {
   try {
-    const response = await fetch(`i18n/${lang}.json?v=2.7`);
+    const response = await fetch(`i18n/${lang}.json?v=2.8`);
     if (!response.ok) {
       throw new Error(`Erro ao carregar tradução: ${response.statusText}`);
     }
